adb958d0c589384ad78a97bb444bbe19ab45c117
GrodVidar <vexxart@gmail.com>
2023-03-17 00:30:58 +0100

    changed how time defaults

--- weather_app/models.py
+++ weather_app/models.py
@@ -5,4 +5,4 @@ from django.utils.timezone import now
 class HeatIndex(models.Model):
     temperature = models.FloatField()
     humidity = models.IntegerField()
-    date = models.DateTimeField(default=now())
+    date = models.DateTimeField(default=now)
